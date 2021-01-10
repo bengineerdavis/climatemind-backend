405b83c337b8993bc5e85de5ed3fe7a7a21cc0d7
Alexis Carras <alexisjcarras@gmail.com>
2021-01-10 17:11:42 +0200

    imolement regressor

--- ml_models/politcial_preference/political_preference_predictor/model.py
+++ ml_models/politcial_preference/political_preference_predictor/model.py
@@ -1,8 +1,6 @@
-""" 
-Implement: LASSO, Logistic Regression, and NB
-
 """
-
+Implement: LASSO, Logistic Regression, and NB
+"""
 import numpy as np
 from sklearn.linear_model import LinearRegression, Lasso
 
@@ -14,22 +12,26 @@ class Model:
 
         if classifier_type == 'LASSO':
             self.model = Lasso(alpha = 1.0) #'alpha' --> lambda value
+            print("Initating LASSO")
         elif classifier_type == 'LinearRegression':
-            self.model =LinearRegression()
+            self.model = LinearRegression()
+            print("Initating Linear Regression")
+
+                                                                #QUESTION for Kameron: What path do I pass?
+        self.X_train, self.X_test, self.y_train, self.y_test = load_data("/Users/alexiscarras/Desktop/cm backend/climatemind-backend/ml_models/politcial_preference/datasets/pvq21CENTRED.csv")
         
     def train(self):
-        
+
         print("training model...")
-        #QUESTION for Kameron: What path do I pass?
-        X_train, X_test, y_train, y_test = load_data("/Users/alexiscarras/Desktop/cm backend/climatemind-backend/ml_models/politcial_preference/datasets/pvq21CENTRED.csv")
-        
+        return self.model.fit(self.X_train, self.y_train)
 
+    def predict(self, X_test = None):
 
-    def predict(self, x_unknown = None):
+        return self.model.predict(self.X_test)
 
-        pass
-
-    def test(self, X_test, y_test): 
-
-        pass 
+    def test(self, X_test = None, y_test = None): 
+        predictions = self.predict()    
+        print(predictions)
+        print("Accuracy of: ", np.mean(predictions.astype(int) == self.y_test.astype(int)))
+        return np.mean(predictions.astype(int) == self.y_test.astype(int))
 
--- ml_models/politcial_preference/political_preference_predictor/pp_predictor.py
+++ ml_models/politcial_preference/political_preference_predictor/pp_predictor.py
@@ -3,16 +3,18 @@ import sys, argparse
 from data_loader import load_data
 from model import Model
 
+
+
+# So far: 
+#    - Linear Regressor accuracy 0.263
+#    - LASSO accuracy 0.334
+
 def main():
     
     model = Model('LASSO')
     model.train()
 
-
-
-
-
-
+    model.test()
 
 
 
